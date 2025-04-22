5d42dbcfdc72bfcae7b24f83c65845f888497c2b
Ehsan-1998 <Faizeeehsan@gmail.com>
2025-04-22 16:40:08 -0400

    Installed the SceneManager and  added  script for  the main_menu.gd

---
+++ scripts/main_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_play_button_pressed() -> void:
+	SceneManager.change_scene("res://scenes/rambo.tscn", {
+		"speed": 5,
+		"pattern": "scribbles",
+	})
