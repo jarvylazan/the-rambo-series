124e0e4052b3e9bb51721fbf6d8646dfa120a3be
Moustapha <MamadouMoustapha.Cou@stu.crc-lennox.qc.ca>
2025-05-02 23:07:32 -0400

    updating level 3

--- scripts/labyrinth_level_3.gd
+++ scripts/labyrinth_level_3.gd
@@ -9,6 +9,12 @@ func _on_section_1_trigger_body_entered(body: Node2D) -> void:
 		print("Locking to Section 1")
 		lock_camera_to_section("Section_1")
 
+func _on_section_2_trigger_body_entered(body: Node2D) -> void:
+	print("Entered by: ", body.name)
+	if body.name == "Player":
+		print("Locking to Section 2")
+		lock_camera_to_section("Section_2")
+
 func lock_camera_to_section(section_name: String):
 	var camera = $Player/Camera2D
 
@@ -19,9 +25,12 @@ func lock_camera_to_section(section_name: String):
 			camera.limit_left = -7
 			camera.limit_right = 1344
 
+		"Section_2":
+			camera.limit_top = -105
+			camera.limit_bottom = 1207
+			camera.limit_left = 1378
+			camera.limit_right = 2724
+
+
 		_:
 			push_error("Unknown section: " + section_name)
-
-
-func _on_section_2_trigger_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
