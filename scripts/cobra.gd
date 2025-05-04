dac0a616c3832b6575d84b499e1d9250d0178c8d
Tarin <mohammadtarin.wahidi@crcmail.net>
2025-05-03 21:53:17 -0400

    patrolling, attacking draft

--- scripts/cobra.gd
+++ scripts/cobra.gd
@@ -1,5 +1,7 @@
 extends Enemy
 
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
+
 func _ready():
 	speed = 40
 	max_health = 30
@@ -8,6 +10,25 @@ func _ready():
 	detection_range = 120.0
 	attack_range = 35.0
 	super()
+	
+func attack() -> void:
+	if is_attacking or is_dead or not can_attack:
+		return
+
+	is_attacking = true
+	can_attack = false
+	anim_player.play("attack")
+	await anim_player.animation_finished
+	is_attacking = false
+
+	await get_tree().create_timer(attack_cooldown).timeout
+	can_attack = true
 
 func take_gun_damage():
 	take_damage(10)
+
+
+func _on_fang_hitbox_body_entered(body: CharacterBody2D) -> void:
+	print("I got something!")
+	if body.is_in_group("player"):
+		Global.take_damage(10)
