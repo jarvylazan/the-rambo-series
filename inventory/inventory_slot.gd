242432f4410edeca4d6c5c2f2d77e4c389d1fc23
Moustapha <MamadouMoustapha.Cou@stu.crc-lennox.qc.ca>
2025-05-07 03:14:59 -0400

    Added the functionality to move items in inventory

--- inventory/inventory_slot.gd
+++ inventory/inventory_slot.gd
@@ -1,45 +1,26 @@
-extends Panel
+extends Button
 
 @onready var item_visual: Sprite2D = $CenterContainer/Panel/ItemDisplay
 @onready var amount_text: Label = $CenterContainer/Panel/Label
 @onready var slot_sprite: Sprite2D = $Sprite2D
 
-var slot_data: InvSlot
+var inventory_slot: InvSlot
 
-func update(slot: InvSlot):
-	slot_data = slot
-	if !slot.item:
+func update_slot():
+	if !inventory_slot or !inventory_slot.item:
 		item_visual.visible = false
 		amount_text.visible = false
 		slot_sprite.frame = 0
 	else:
 		item_visual.visible = true
-		item_visual.texture = slot.item.texture
+		item_visual.texture = inventory_slot.item.texture
 		slot_sprite.frame = 1
-		amount_text.visible = slot.amount > 1
-		amount_text.text = str(slot.amount)
+		if inventory_slot.amount > 1:
+			amount_text.visible = true
+			amount_text.text = str(inventory_slot.amount)
+		else:
+			amount_text.visible = false
 
-# Step 1: Start drag
-func get_drag_data(_position):
-	if slot_data and slot_data.item:
-		var preview = TextureRect.new()
-		preview.texture = slot_data.item.texture
-		preview.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
-		preview.custom_minimum_size = Vector2(32, 32)
-		set_drag_preview(preview)
-		return slot_data
-
-# Step 2: Accept drag
-func can_drop_data(_position, data):
-	return data is InvSlot
-
-# Step 3: Handle drop
-func drop_data(_position, data):
-	if data is InvSlot:
-		var temp_item = slot_data.item
-		var temp_amount = slot_data.amount
-		slot_data.item = data.item
-		slot_data.amount = data.amount
-		data.item = temp_item
-		data.amount = temp_amount
-		get_tree().call_group("InventoryUI", "update_slots")
+func _pressed():
+	# Let the InventoryUI handle the logic
+	get_tree().call_group("InventoryUI", "slot_clicked", self)
--- inventory/inventory_ui.gd
+++ inventory/inventory_ui.gd
@@ -3,6 +3,10 @@ class_name InventoryUI  # 👈 Needed for call_group
 
 @onready var inv: Inv = preload("res://inventory/player_inv.tres")
 @onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+var selected_slot_gui: Button = null
+
+var selected_slot_data: InvSlot = null
+
 
 var is_open = false
 
@@ -14,7 +18,9 @@ func _ready():
 
 func update_slots():
 	for i in range(min(inv.slots.size(), slots.size())):
-		slots[i].update(inv.slots[i])
+		slots[i].inventory_slot = inv.slots[i]
+		slots[i].update_slot()
+
 
 func _process(delta):
 	if Input.is_action_just_pressed("Inventory"):
@@ -30,3 +36,44 @@ func close():
 func open():
 	visible = true
 	is_open = true
+
+func slot_clicked(slot_gui):
+	var clicked_index = slots.find(slot_gui)
+
+	if selected_slot_gui == null:
+		# First click: pick up the item from the clicked slot
+		if slot_gui.inventory_slot and slot_gui.inventory_slot.item:
+			selected_slot_gui = slot_gui
+			selected_slot_data = slot_gui.inventory_slot
+			if clicked_index != -1:
+				inv.slots[clicked_index] = InvSlot.new()  # Clear in model
+			slot_gui.inventory_slot = null
+			slot_gui.update_slot()
+
+	else:
+		var selected_index = slots.find(selected_slot_gui)
+
+		if clicked_index == selected_index:
+			# Cancel selection if same slot is clicked again
+			selected_slot_gui.inventory_slot = selected_slot_data
+			if selected_index != -1:
+				inv.slots[selected_index] = selected_slot_data
+			selected_slot_gui.update_slot()
+		elif clicked_index != -1 and selected_index != -1:
+			# Swap if both slots contain data
+			var target_data: InvSlot = slot_gui.inventory_slot
+			slot_gui.inventory_slot = selected_slot_data
+			selected_slot_gui.inventory_slot = target_data
+
+			# Sync to model
+			inv.slots[clicked_index] = selected_slot_data
+			inv.slots[selected_index] = target_data
+
+			slot_gui.update_slot()
+			selected_slot_gui.update_slot()
+
+		# Reset selection
+		selected_slot_gui = null
+		selected_slot_data = null
+
+	inv.update.emit()
