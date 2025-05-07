bb7a2e3eb69ace5409928988b6cbd2ace1d9cbe6
Tarin <mohammadtarin.wahidi@crcmail.net>
2025-05-06 21:20:52 -0400

    akaname, intel devourer, kobold priest

---
+++ scripts/intellect_devourer.gd
@@ -0,0 +1,21 @@
+extends Enemy
+
+func _ready():
+	speed = 150
+	max_health = 30
+	health = max_health
+	damage = 8
+	detection_range = 450.0
+	attack_range = 60.0
+	super()
+
+
+func take_gun_damage():
+	take_damage(10)
+	
+func take_spear_damage():
+	take_damage(30)
+
+func _on_attack_hitbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		Global.take_damage(10)
